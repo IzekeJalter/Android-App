fd1305c6ebe2332aff9f317a27c8ac63085e6901
Victor <winnie_puh16@outlook.com>
2022-12-13 15:23:23 -0600

    CrearParque

--- app/src/main/java/com/example/vida1/CrearParque.java
+++ app/src/main/java/com/example/vida1/CrearParque.java
@@ -6,8 +6,17 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class CrearParque extends AppCompatActivity {
+
+    public EditText nombreParque, largoTerreno, anchoTerreno, reglas;
+    public Button btnCrearParque;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,19 @@ public class CrearParque extends AppCompatActivity {
         getSupportActionBar().hide();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         findViewById(R.id.btnregresar).setOnClickListener(this::RegresarPagina);
+
+        nombreParque = (EditText)findViewById(R.id.EditTextCrearNombreParque);
+        largoTerreno = (EditText) findViewById(R.id.EditTextCrearLargoTerreno);
+        anchoTerreno = (EditText) findViewById(R.id.EditTextCrearAnchoTerreno);
+        reglas = (EditText) findViewById(R.id.EditTextCrearReglasParque);
+        btnCrearParque = (Button) findViewById(R.id.btnCrearParque);
+        requestQueue = Volley.newRequestQueue(this);
+        btnCrearParque.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     private void RegresarPagina(View view) {
